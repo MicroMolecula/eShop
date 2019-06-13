768a95973526e055021eec75e1dc29829f96a21b
Den4ick <den4ik@gmail.com>
2019-06-13 17:51:08 +0300

    Added logic for Added logic for sending email to user

--- common/models/order.js
+++ common/models/order.js
@@ -2,13 +2,25 @@ module.exports = async (Order) => {
     Order.observe('before save', async (ctx) => {
         const cart = await app.models.Cart.findOne({ where: { userId: ctx.instance.ownerId } });
         const cartItem = await app.models.CartItem.find({ where: { cartId: cart.id } });
-    
+        const user = await app.models.User.findById(ctx.instance.userId);
+        const currentDate = new Date();
+
         for (let item of cartItem) {
-          ctx.instance.totalSum += item.totalSum;
-    
-          item.cartId = null;
-          cart.totalSum = 0;
-          await item.save();
+            ctx.instance.totalSum += item.totalSum;
+
+            item.cartId = null;
+            cart.totalSum = 0;
+            await item.save();
         }
-      });
+
+        await Order.app.models.Email.send({
+            to: user.email,
+            from: '',
+            subject: 'Internet shop',
+            text: '',
+            html: "<p> Hello  " + user.username + " \n Thanks for shopping in my shop. \n Your order sum is "
+                + ctx.instance.totalSum + currentDate + "</p>"
+        })
+        
+    });
 };
