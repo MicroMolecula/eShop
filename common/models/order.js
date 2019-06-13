565f74899283e70be8c78d5a28a814767fea726f
Den4ick <den4ik@gmail.com>
2019-06-13 17:46:13 +0300

    Added logic for order totalSum calculation

--- common/models/order.js
+++ common/models/order.js
@@ -1,2 +1,14 @@
 module.exports = async (Order) => {
+    Order.observe('before save', async (ctx) => {
+        const cart = await app.models.Cart.findOne({ where: { userId: ctx.instance.ownerId } });
+        const cartItem = await app.models.CartItem.find({ where: { cartId: cart.id } });
+    
+        for (let item of cartItem) {
+          ctx.instance.totalSum += item.totalSum;
+    
+          item.cartId = null;
+          cart.totalSum = 0;
+          await item.save();
+        }
+      });
 };
