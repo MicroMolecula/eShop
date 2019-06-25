55ef3169c22ed42f75c15563ac00419d24e1aa7a
Den4ick <den4ik@gmail.com>
2019-06-25 18:03:59 +0300

    Made order method custom

--- common/models/order.js
+++ common/models/order.js
@@ -1,20 +1,23 @@
 module.exports = async (Order) => {
-    Order.observe('before save', async (ctx) => {
-        const cart = await app.models.Cart.findOne({ where: { userId: ctx.instance.ownerId } });
+    Order.makeOrder = async (userId) => {
+        const cart = await app.models.Cart.findOne({ where: { userId: userId } });
         const cartItem = await app.models.CartItem.find({ where: { cartId: cart.id } });
-        const user = await app.models.User.findById(ctx.instance.userId);
+        const user = await app.models.User.findById(userId);
         const currentDate = new Date();
+        const order = await Order.create({});
 
         for (let item of cartItem) {
-
-            ctx.instance.totalSum += item.totalSum;
+            order.totalSum += item.totalSum;
+            item.id = order.id;
             item.cartId = null;
             cart.totalSum = 0;
-            
+
             await item.save();
         }
-        
         await cart.save();
+        await order.save();
+
+        return 'Order has made';
 
         await Order.app.models.Email.send({
             to: user.email,
@@ -24,6 +27,12 @@ module.exports = async (Order) => {
             html: "<p> Hello  " + user.username + " \n Thanks for shopping in my shop. \n Your order sum is "
                 + ctx.instance.totalSum + " " + currentDate + "</p>"
         })
-
+    }
+    Order.remoteMethod('makeOrder', {
+        'descriptions': 'Make order',
+        accepts:
+            { arg: 'userId', type: 'string' },
+        returns: { arg: 'message', type: 'string' },
+        http: { verb: 'post' }
     });
 };
